83563206cfccdfcf1a783671621cb0d6f19c8b19
Diyorbek <85053051+ravshanov1003@users.noreply.github.com>
2021-06-16 18:42:25 +0500

    delete

--- portfolio/index.js
+++
@@ -1,5 +0,0 @@
-let toggleBtn = document.getElementsByClassName("toggle-btn")[0]
-let right = document.getElementsByClassName("right")[0]
-toggleBtn.addEventListener('click', () => {
-    right.classList.toggle("active");
-})
